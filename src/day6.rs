5657084efe17eac4accc6c3e67f892e9d8881fc3
Carl 'Subzidion' Hiltbrunner <git@carl.hiltbrunner.email>
2023-12-05 22:39:41 -0800

    Day 6, Part 2 implementation.

--- src/day6.rs
+++ src/day6.rs
@@ -24,3 +24,27 @@ pub fn part1(input: &str) -> u32 {
 
     total
 }
+
+#[aoc(day6, part2)]
+pub fn part2(input: &str) -> u32 {
+    let race_1: (u64, u64) = (42686985, 284100511221341);
+    let races: Vec<(u64, u64)> = vec![race_1];
+
+    let mut total = 1;
+
+    for race in races {
+        let mut race_wins: u32 = 0;
+        for hold_time in 0..race.0 {
+            let time_left = race.0 - hold_time;
+            let distance = hold_time * time_left;
+
+            if distance > race.1 {
+                race_wins += 1;
+            }
+        }
+
+        total *= race_wins;
+    }
+
+    total
+}
